13be6bcbd6d322a9e1e2ae382527f09299fd5895
Gabriel <gabriel58221@gmail.com>
2024-11-08 22:12:34 -0300

    docker file configurado com o banco de dados

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+import 'express-async-errors'
+import { errorHandling } from './infrastructure/middlewares/error-handling';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(errorHandling);
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
