94cf82faf77892dd7b3d2e166c9cca6931884659
Gabriel <gabriel58221@gmail.com>
2025-01-02 21:11:05 -0300

    coleta de coordenadas das obras criada

---
+++ backend/node/prisma/migrations/20250102193358_mudando_codigo/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Executor" ALTER COLUMN "codigo" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Repassador" ALTER COLUMN "codigo" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Tomador" ALTER COLUMN "codigo" SET DATA TYPE TEXT;
