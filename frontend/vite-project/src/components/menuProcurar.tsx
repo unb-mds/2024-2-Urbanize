6f1b4c437f6dad5b1e2c57fcdf2990e88114e1ee
Leo Melo <leonardolimabranco@gmail.com>
2025-01-13 16:11:12 -0300

    [feat](#28): Implementar svg lupa na barra de pesquisar do menu procurar

--- frontend/vite-project/src/components/menuProcurar.tsx
+++ frontend/vite-project/src/components/menuProcurar.tsx
@@ -23,11 +23,16 @@ const MenuProcurar: React.FC<MenuProcurarProps> = ({
 
         {/* Campo de busca */}
         <div className="w-full flex-grow flex flex-col p-3 bg-white mt-[-5px] overflow-y-auto">
-          <input
-            type="text"
-            placeholder="Cidade, tipo de obra"
-            className="w-full px-3 py-2 text-gray-700 bg-customBlue text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-          />
+          <div className="flex items-center bg-customBlue rounded-[10px] px-4 py-3">
+            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26" fill="none">
+              <path d="M17.6389 17.658L24 24M20.3333 11.1667C20.3333 16.2292 16.2292 20.3333 11.1667 20.3333C6.10405 20.3333 2 16.2292 2 11.1667C2 6.10405 6.10405 2 11.1667 2C16.2292 2 20.3333 6.10405 20.3333 11.1667Z" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            <input
+              type="text"
+              placeholder="Cidade, tipo de obra"
+              className="w-full text-gray-100 bg-customBlue text-white focus:outline-none ml-3"
+            />
+          </div>
         </div>
 
         {/* Botões fixos na parte inferior */}
