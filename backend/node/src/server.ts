c83659bdcb7bee3c33592cd2f3be78ba54d21b87
Gabriel Lima <gabriel58221@gmail.com>
2025-01-14 00:05:42 -0300

    Update server.ts

--- backend/node/src/server.ts
+++ backend/node/src/server.ts
@@ -23,7 +23,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // inicializando o cron job para coleta de dados
 new CronJob( 
-  '0 0 */7 * *', // atualiza a cada 7 dias
+  '* * * * *', // atualiza a cada 7 dias
   async () => { 
     await fetchAndSaveProjects(); 
   }, 
