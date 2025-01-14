e04d75c54dffa0d3c47cfb79fef0bd5a3ef66c43
angelicaccampos <angelicadacostacampos@gmail.com>
2025-01-14 08:46:19 -0300

    [feat](#28): Adiciona função de abertura do detalhar

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -22,7 +22,8 @@ const App: React.FC = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <MapComponent openDetailMenu={openDetailMenu} />
-      <MenuComponent />
+      {/* Renderiza o MenuPrincipal apenas quando o menu de detalhamento não está aberto */}
+      {!isDetailMenuOpen && <MenuComponent />}
       {isDetailMenuOpen && (
         <MenuDetalhar closeDetailMenu={closeDetailMenu} obraDetalhada={selectedDetail!} />
       )}
