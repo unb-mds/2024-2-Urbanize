c106171078a4b0c8216808058ab5ec8cf13fdf74
Gabriel Lima <gabriel58221@gmail.com>
2025-01-14 00:12:43 -0300

    [fix](#21): Mudanas de parâmetros do cron job

--- backend/node/src/server.ts
+++ backend/node/src/server.ts
@@ -23,7 +23,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // inicializando o cron job para coleta de dados
 new CronJob( 
-  '* * * * *', // atualiza a cada 7 dias
+  '0 0 */7 * *', // atualiza a cada 7 dias
   async () => { 
     await fetchAndSaveProjects(); 
   }, 
