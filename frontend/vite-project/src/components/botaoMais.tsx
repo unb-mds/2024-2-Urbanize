97dc9c3981a559a4e4be65be946ba5b371a80c63
angelicaccampos <angelicadacostacampos@gmail.com>
2025-02-05 20:04:34 -0300

    [refact]: Adiciona logo dentro do botão 'Saiba Mais' e ajusta responsividade"

--- frontend/vite-project/src/components/botaoMais.tsx
+++ frontend/vite-project/src/components/botaoMais.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 
 const BotaoSaibaMais: React.FC = () => {
     return (
-        <a 
-            href="https://github.com/unb-mds/2024-2-Urbanize" 
-            target="_blank" 
+        <a
+            href="https://github.com/unb-mds/2024-2-Urbanize"
+            target="_blank"
             rel="noopener noreferrer"
-            className="fixed z-10 px-[7px] py-2 p-5 mt-3 text-[20px] ml-12 rounded-[10px] bg-customBlue text-white  hover:bg-blue-600 transition-colors duration-300"
+            className="fixed z-10 flex mt-[-10px] flex-col items-center ml-11 text-[15px] font-bold text-customBlue transition-colors duration-300 hover:scale-110 sm:text-[60px] md:text-[30px] lg:text-[15px]"
+
         >
-            Saiba Mais
+            <img
+                src="/logoUrbanize.svg"
+                alt="Urbanize Logo"
+                style={{ width: '140px', height: '140px' }} 
+                className=" transition-transform duration-300 hover:scale-110 "
+
+            />
+            <span className="font-mono mt-[-40px]">Saiba Mais</span>
         </a>
     );
 };
